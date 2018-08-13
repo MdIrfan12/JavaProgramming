e3f98aaacf40d64b6c59d326cd4869526c86ac7a
MdIrfan12 <42328008+MdIrfan12@users.noreply.github.com>
2018-08-13 06:11:09 +0530

    program to yunderstand ArrayList
    It is the concept of Collection.

---
+++ arraylist.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class arraylist
+{
+	public static void main(String... args)throws Exception
+	{
+		LinkedList ar=new LinkedList();
+		ar.add("php");
+        Object string=ar.getFirst(); 
+		System.out.println(string);
+	}
+}
